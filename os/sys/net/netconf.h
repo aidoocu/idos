df624c766267f847bb8a04269cbb5664e1d0e53e
Bernardo Leon <bernardoyla@gmail.com>
2022-03-30 11:43:20 -0400

    use native config

--- os/sys/net/netconf.h
+++ os/sys/net/netconf.h
@@ -8,7 +8,7 @@
 #define MAC_LEN 6
 //#define MAC_ADDRESS 0xde, 0xad, 0xbe, 0xef, 0xfe, 0xed
 
-//#define NET_STATIC
+#define NET_STATIC
 
 #if CONF_NET == NET_IPV4
 
@@ -16,20 +16,20 @@
 
 #ifdef NET_STATIC
 /* Config static by default*/
-#define IP_ADDRESS 10, 16, 21, 155
+/* #define IP_ADDRESS 10, 16, 21, 155
 #define IP_GATEWAY 10, 16, 21, 1
 //#define IP_SUBNET 10, 16, 21, 1
-#define IP_MASK 255, 255, 255, 0
+#define IP_MASK 255, 255, 255, 0 */
 
 /* #define IP_ADDRESS 192, 168, 1, 115
 #define IP_GATEWAY 192, 168, 1, 1
 #define IP_SUBNET 192, 168, 1, 0
 #define IP_MASK 255, 255, 255, 0 */
 
-/* #define IP_ADDRESS 172, 18, 0, 5
+#define IP_ADDRESS 172, 18, 0, 5
 #define IP_GATEWAY 172, 18, 0, 1
 #define IP_SUBNET 172, 18, 0, 0
-#define IP_MASK 255, 255, 0, 0 */
+#define IP_MASK 255, 255, 0, 0
 
 #endif /* NET_STATIC */
 
