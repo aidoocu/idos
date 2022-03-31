c811a8259e0be07a9e925132c17bb39773b7240b
Bernardo Leon <bernardoyla@gmail.com>
2022-03-31 12:43:25 -0400

    some reordering and add coap_link_st

--- apps/coap/coap.h
+++ apps/coap/coap.h
@@ -7,6 +7,9 @@
 
 #include "../../os/idos.h"
 
+/** 
+ * \brief Estructura para el parceo del header de CoAP
+ */
 struct coap_hdr_st{
     uint8_t ver_t_tkl;      /* Version, type and Token len */
     uint8_t code;           /* Respose code */
@@ -18,7 +21,7 @@ struct coap_hdr_st{
  *  \param hdr Estructura del header de CoAP
  *  \param buf Buffer donde está el mensaje CoAP
  */
-#define coap_hdr(hdr, buf) coap_hdr_st * hdr = (struct coap_hdr_st *)&buf
+#define coap_hdr(hdr, buf) hdr = (struct coap_hdr_st *)&buf
 
 struct coap_payload_st {
     uint8_t * rcvd;                     /* Message payload */
@@ -52,6 +55,16 @@ struct coap_payload_st {
  * \brief Optener el option number
  */
 #define option_number() ((coap_listener.msg_in[cp] & 0xF0) >> 4) + option_delta 
+
+
+/** 
+ * \brief Responder inmeditamente.
+ * \details Responder implica que ................
+ * mismo token?
+ */
+#define coap_respond(type, code)      \
+    coap_response(type, code, &coap_listener);
+
 /* --------------------------------------------------------------------------------- */
 
 
@@ -60,11 +73,26 @@ struct coap_payload_st {
  */
 void coap_start(void);
 
+/**
+ * \brief   Estructura para referirse al enlace del recurso remoto
+ * \details Sigue el esquema URI de CoAP:
+ *  coap://<host>:<port>/<path>/?<request>
+ * 
+ */
+struct coap_link_t {
+    ip_address_t host;  /* IP del extremo */
+    uint16_t port;      /* puerto del extremo */
+    char * path;        /* recurso dentro del host */
+    char * request;     /* Peticio al recurso */
+};
 
-/*  */
-#define coap_respond(type, code)      \
-    coap_response(type, code, &coap_listener, coap_rcvd_hdr);
+/** 
+ *  \brief Envía un paquete CoAP a un remoto sin conexión
+ *      
+ */
+bool coap_send_non(struct coap_link_t * link, char * msg, uint8_t len);
 
+//bool coap_send_con();
 
 #include "coap_const.h"
 #include "coap_resource.h"
