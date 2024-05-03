4d2c80985d8e99137cbd26cb1d22f495b28ff562
Bernardo Yaser León Ávila <bernardoyla@gmail.com>
2024-05-03 18:41:26 +0300

    Refactor sleep architecture implementation for AVR ATmega328P

--- os/arch/cpu/avr/atmega328p/sleep_arch.cpp
+++ os/arch/cpu/avr/atmega328p/sleep_arch.cpp
@@ -1,7 +1,40 @@
-/*  */
-#include "sleep_arch.h"
+/** 
+ * @file sleep_arch.cpp
+ * @brief Implementation of sleep architecture functions for AVR ATmega328P.
+ * 
+ * Each sleep/wake cycle will be considered a system tick. Remember that normal 
+ * timers work by time expiration detection so not real time. To sleep/wake up, 
+ * the WDT is activated for the length of time you want the cycle to last and 
+ * you go to sleep. Once the WDT expires and causes an interruption, this is 
+ * attended to in the WDT call back and disables it so that mote does what it 
+ * needs to do until the scheduler voluntarily puts it to sleep.
+ * 
+ * The 
+ */
 
+#include "sleep_arch.h"
+#include "../lib/low_power/LowPower.h"
+
+/**
+ * @brief External variable representing the value of the timer0 milliseconds counter.
+ * 
+ * This variable is used to keep track of the number of milliseconds elapsed since the program started.
+ * It is updated by the timer0 interrupt handler. This variable is declared as extern because the original
+ * ones is implemented in the Arduino framework, and we don't want to modify it.
+ */
+extern unsigned long timer0_millis;
+
+#define SLEEP_TIME SLEEP_2S
+
+/**
+ * @brief Initializes the sleep architecture.
+ * 
+ * This function sets the sleep mode to SLEEP_MODE_IDLE.
+ * 
+ * @note This function needs to be reconfigurable for testing purposes.
+ */
 void sleep_init_arch(void){
+
     /*  SLEEP_MODE_PWR_DOWN
         SLEEP_MODE_PWR_SAVE
         SLEEP_MODE_STANDBY
@@ -9,56 +42,102 @@ void sleep_init_arch(void){
         SLEEP_MODE_IDLE
         SLEEP_MODE_ADC
     */
-    set_sleep_mode(SLEEP_MODE_IDLE);
+    //set_sleep_mode(SLEEP_MODE_IDLE);
 
-    /* A partir de aquí hay que hacer reconfigurable esto para test */
- 
 }
 
+/**
+ * @brief Puts the CPU into sleep mode.
+ * 
+ * This function disables power to all peripherals except Timer 0,
+ * enables the Watchdog Timer to wake up the CPU after a specified time,
+ * and puts the CPU into sleep mode.
+ * 
+ * @note The Watchdog Timer generates an interrupt and does not reset the system.
+ */
 void sleep_arch(void){
 
-    /** Apagar todo menos el Timer 0 */
-    power_disable();
+    //Serial.print("S: ");
+    //Serial.println(millis());
 
-    /** Habilitar el WDT para que despierte a la cpu dentro de:
-     * WDTO_15MS
-     * WDTO_30MS
-     * WDTO_60MS
-     * WDTO_120MS
-     * WDTO_250MS
-     * WDTO_500MS
-     * WDTO_1S
-     * WDTO_4S
-     * WDTO_8S
-     */
-    wdt_enable(WDTO_15MS);
-    wdt_reset();
-    /** El WDT genera una interrupción y no resetea el sistema */
-	WDTCSR |= (1 << WDIE);
+    //delay(10);
 
-    cli();
-    /* Set SE (sleep enable) bit */
-    sleep_enable();
-    /* desabilitar el brown-out detection */
-    sleep_bod_disable();
-    sei();
+    LowPower.powerDown(SLEEP_TIME, ADC_OFF, BOD_OFF);
 
-    /* Sleep */
-    sleep_cpu();
+    /** Cuando se va a dormir los temporizadores se duermen y se deja de contar 
+     * el tiempo por lo que hay que "poner en hora" los registros de millis().
+     * para ello se ha declado como extern en este archivo la variable timer0_millis
+     * que es la que lleva la cuenta de los milisegundos del sistema.
+    **/
+    uint8_t oldSREG = SREG;
+	// disable interrupts while we read timer0_millis or we might get an
+	// inconsistent value (e.g. in the middle of a write to timer0_millis)
+	cli();
+    
+    /** @todo esto hay que hacerlo funcionar bien    */
+    /*#if SLEEP_TIME == SLEEP_15MS
+    timer0_millis = timer0_millis + 15;
+    #elif SLEEP_TIME == SLEEP_30MS
+    timer0_millis = timer0_millis + 30;
+    #elif SLEEP_TIME == SLEEP_60MS
+    timer0_millis = timer0_millis + 60;
+    #elif SLEEP_TIME == SLEEP_120MS
+    timer0_millis = timer0_millis + 120;
+    #elif SLEEP_TIME == SLEEP_250MS
+    timer0_millis = timer0_millis + 250;
+    #elif SLEEP_TIME == SLEEP_500MS
+    timer0_millis = timer0_millis + 500;
+    #elif SLEEP_TIME == SLEEP_1S
+    timer0_millis = timer0_millis + 1000;
+    #elif SLEEP_TIME == SLEEP_2S
+    timer0_millis = timer0_millis + 2000;
+    #elif SLEEP_TIME == SLEEP_4S
+    timer0_millis = timer0_millis + 4000;
+    #elif SLEEP_TIME == SLEEP_8S
+    timer0_millis = timer0_millis + 8000;
+    #endif */
 
-    /* Desabilitar los modos de sleep */
-    sleep_disable();
+    if(SLEEP_TIME == SLEEP_15MS)
+        timer0_millis = timer0_millis + 15;
+    else if(SLEEP_TIME == SLEEP_30MS)
+        timer0_millis = timer0_millis + 30;
+    else if(SLEEP_TIME == SLEEP_60MS)
+        timer0_millis = timer0_millis + 60;
+    else if(SLEEP_TIME == SLEEP_120MS)
+        timer0_millis = timer0_millis + 120;
+    else if(SLEEP_TIME == SLEEP_250MS)
+        timer0_millis = timer0_millis + 250;
+    else if(SLEEP_TIME == SLEEP_500MS)
+        timer0_millis = timer0_millis + 500;
+    else if(SLEEP_TIME == SLEEP_1S)
+        timer0_millis = timer0_millis + 1000;
+    else if(SLEEP_TIME == SLEEP_2S)
+        timer0_millis = timer0_millis + 2000;
+    else if(SLEEP_TIME == SLEEP_4S)
+        timer0_millis = timer0_millis + 4000;
+    else if(SLEEP_TIME == SLEEP_8S)
+        timer0_millis = timer0_millis + 8000;
+        
 
-    /* Volver a encender todo */
-    power_enable();
+	SREG = oldSREG;
 
-    sei();
+    //delay(10);
+
+    //Serial.print("W: ");
+    //Serial.println(millis());
 }
 
 // Temporary clock source variable ¿volatile?
 unsigned char clockSource = 0;
 
-/* Aquí se habilita todo sin exepción, habria que ver que conviene y que no en realidad */
+/**
+ * @brief Enables power to all peripherals.
+ * 
+ * This function restores the previous clock source setting for Timer 2,
+ * enables power to Timer 2, ADC, Timer 1, SPI, USART0, and TWI.
+ * 
+ * @note This function enables all peripherals without exception.
+ */
 void power_enable(void){
     // Restore previous setting
     TCCR2B = clockSource;
@@ -75,8 +154,15 @@ void power_enable(void){
     power_twi_enable();    
 }
 
-
-/* Aquí se desabilita todo sin exepción, habria que ver que conviene y que no en realidad */
+/**
+ * @brief Disables power to all peripherals.
+ * 
+ * This function stores the current clock source setting for Timer 2,
+ * removes the clock source to shutdown Timer 2, disables power to Timer 2,
+ * ADC, Timer 1, SPI, USART0, and TWI.
+ * 
+ * @note This function disables all peripherals without exception.
+ */
 void power_disable(void){
 
     // Store current setting
@@ -99,9 +185,15 @@ void power_disable(void){
 	power_twi_disable();
 }
 
-
-ISR (WDT_vect)
-{
+/**
+ * @brief Watchdog Timer interrupt service routine.
+ * 
+ * This function is executed when the Watchdog Timer times out.
+ * It disables the Watchdog Timer.
+ */
+/* ISR (WDT_vect) {
 	// WDIE & WDIF is cleared in hardware upon entering this ISR
 	wdt_disable();
-}
+    //printf("WDT\n");
+    Serial.println("WDT");
+} */
